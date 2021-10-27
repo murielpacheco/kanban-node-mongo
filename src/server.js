f54afa6bbf4ef82b411dcec4fd209b907b353baa
Muriel Damazio <murieldps@gmail.com>
2021-10-27 17:34:53 -0300

    Installing CORS. API done!

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const connectToDatabase = require('./database');
-
+const cors = require('cors');
 connectToDatabase();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
